531a9c5c823a47edcdf71de8b91860ee5a616dd0
John Yeilding <000850543@mohawkcollege.ca>
2024-09-12 10:50:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+    namespace textgame
+    {
+         class Program
+    { 
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my role-playing text game");
+            Console.WriteLine("The first room in this text playing game is to answer a math question, 2+2=what");
+                String input = Console.readline();
+            if (input.ToLower() == "Answer the question")
+                {
+                Console.WriteLine("You have solved the first question, and you unlocked level 2");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Command.");
+            }
+        }
+    }
+}
+
